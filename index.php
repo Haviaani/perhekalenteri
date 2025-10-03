acdb7f6b713465591b2ef763f5bacdc4d5bcdf91
Aki Haviala <p89565@edu.sasky.fi>
2025-10-03 12:46:15 +0300

    Lomakekentät asemoitu

--- index.php
+++ index.php
@@ -16,41 +16,44 @@
     </header>
     <section>
 
-      <form action="kalenteri.php" method="GET" target="_blank">
+      <form class="form" action="kalenteri.php" method="GET" target="_blank">
 
-        Vuosi: <input type="number" name="year" value="<?php echo date("Y"); ?>"><br>
+      <div class="form_grid">
+        <div class="form_left">Vuosi:</div> 
+        <div class="form_right"><input type="number" name="year" value="<?php echo date("Y"); ?>"></div>
 
-        Kuukausi:
-        <select name="month">
+        <div class="form_left">Kuukausi:</div>
+        <div class="form_right"><select name="month">
         <?php
           foreach($months as $key => $value) {
             echo "<option value='$key'>$value</option>\n";
           }
         ?>
-        </select><br>
+        </select></div>
 
-        Otsikkofontti:
-        <select name="header">
+        <div class="form_left">Otsikkofontti:</div>
+        <div class="form_right"><select name="header">
         <?php
           foreach($headerfonts as $key => $value) {
             echo "<option value='$key'>$value[name]</option>\n";
           }
         ?>
-        </select><br>
+        </select></div>
 
-        Kuva:
-        <select name="bgimage">
+        <div class="form_left">Kuva:</div>
+        <div class="form_right"><select name="bgimage">
         <?php
           foreach ($bgimages as $key => $value) {
             echo "<option value='$key'>$value[name]</option>\n";
           }
         ?>
-        </select><br>
+        </select></div>
 
-        Perheenjäsenet:
-        <textarea name="names" rows="5"><?= $defaultnames ?></textarea><br>
-
-        <input type="submit" value="Avaa kalenterisivu">
+        <div class="form_left">Perheenjäsenet:</div>
+        <div class="form_right"><textarea name="names" rows="5"><?= $defaultnames ?></textarea></div>
+      </div>
+      
+        <div class="form_button"><input type="submit" value="Avaa kalenterisivu"></div>
 
       </form>
     </section>
